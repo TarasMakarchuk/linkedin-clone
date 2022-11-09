53154227f8602afac02895e5dea2d70ea5a5099c
Taras Makarchuk <2278452@gmail.com>
2022-11-09 19:31:56 +0200

    add typeorm, postgres, create .env file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
