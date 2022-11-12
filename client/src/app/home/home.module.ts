c00b0e11f7fc6f175ca9abd3aee4ff24cb5bbd4c
Taras Makarchuk <2278452@gmail.com>
2022-11-12 16:00:18 +0200

    layout header component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
@@ -14,6 +14,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    HeaderComponent,
+  ]
 })
 export class HomePageModule {}
+
