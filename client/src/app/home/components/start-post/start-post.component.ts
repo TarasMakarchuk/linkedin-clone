e1e5762bd89e2acdb2a76bce561fc563c9c12969
Taras Makarchuk <2278452@gmail.com>
2022-11-15 21:11:31 +0200

    initialize start-post component

---
+++ client/src/app/home/components/start-post/start-post.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-post',
+  templateUrl: './start-post.component.html',
+  styleUrls: ['./start-post.component.scss'],
+})
+export class StartPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
