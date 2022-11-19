9910d19bf0fd7029c89b9e56776ba1f7f653cffd
Taras Makarchuk <2278452@gmail.com>
2022-11-19 15:50:11 +0200

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(PORT);
   Logger.log(`Server start on host http://localhost:${PORT}/api`)
