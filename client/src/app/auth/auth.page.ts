d93e7232dfa9ba7a3b5ba660f527120cab4ced7c
Taras Makarchuk <2278452@gmail.com>
2022-11-21 18:57:56 +0200

    :memo:  layout auth page

--- client/src/app/auth/auth.page.ts
+++ client/src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
+  @ViewChild('form') form: NgForm;
+  submissionType: 'login' | 'join' = 'login';
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+
+  };
+
+  toggleText() {
+    this.submissionType = this.submissionType === 'login' ? 'join' : 'login';
+  };
 }
