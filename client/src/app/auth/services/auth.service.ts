6dcd15d3798b2ba71600e0d2930cabaf08af5917
Taras Makarchuk <2278452@gmail.com>
2022-11-30 17:11:48 +0200

    :rocket:  add user models and user registration functionality

--- client/src/app/auth/services/auth.service.ts
+++ client/src/app/auth/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { NewUser } from '../models/new-user.model';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  register(user: NewUser): Observable<User> {
+    return this.http.post<User>(
+      `${environment.baseApiUrl}/auth/registration`,
+      user,
+      this.httpOptions,
+    )
+      .pipe(take(1));
+  };
+
 }
