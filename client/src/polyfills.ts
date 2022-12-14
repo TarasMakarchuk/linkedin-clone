a83faba1e6b075d6b27222a659bb509e9a3fec8d
Taras Makarchuk <2278452@gmail.com>
2022-12-14 14:29:52 +0200

    :rocket: add upload file method in profile-summary.component.ts, add Buffer in global scope (window) in polyfills.ts, decrease version file-type from 18.0.0 to 16.5.4, add file upload input layout, add "browser" settings in package.json

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -63,3 +63,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).global = window;
+//@ts-ignore
+window.Buffer = window.Buffer || require('buffer').Buffer;
