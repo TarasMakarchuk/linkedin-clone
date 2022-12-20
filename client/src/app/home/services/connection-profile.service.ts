90759e685fc4a4157e7c23c15e74794292bfd060
Taras Makarchuk <2278452@gmail.com>
2022-12-20 14:35:10 +0200

    :construction: add details user profile page, methods get user by id in connection-profile.component.ts, remove banner colors from profile-summary.component.ts to banner-color.service.ts file, remove field password in user.model.ts file

--- client/src/app/home/services/connection-profile.service.ts
+++ client/src/app/home/services/connection-profile.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { User } from '../../auth/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionProfileService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getConnectionUser(id: number): Observable<User> {
+    return this.http.get<User>(`${environment.baseApiUrl}/user/${id}`);
+  };
+
 }
