caf801b00a3b9dce9f16b5e9f4f343bb069aa1a4
Taras Makarchuk <2278452@gmail.com>
2022-12-27 21:49:30 +0200

    :arrow_up: refactor home page, connection-profile and user-profile components

--- client/src/app/home/home-routing.module.ts
+++ client/src/app/home/home-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 import { ConnectionProfileComponent } from './components/connection-profile/connection-profile.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: '',
+        component: UserProfileComponent,
+      },
+      {
+        path: ':id',
+        component: ConnectionProfileComponent,
+      },
+    ],
   },
-  {
-    path: ':id',
-    component: ConnectionProfileComponent,
-  },
+
 ];
 
 @NgModule({
