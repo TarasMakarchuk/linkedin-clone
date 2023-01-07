bab96b74ae31c6b3978e011f2c9bfab4bac05332
Taras Makarchuk <2278452@gmail.com>
2023-01-07 15:14:48 +0200

    :construction:  add api error log file, install morgan, class-validator and class-transformer packages

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as morgan from 'morgan';
+import { stream } from 'file-type';
+
+const logStream = fs.createWriteStream('api.log', {
+  flags: 'a', //append
+});
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
@@ -8,6 +15,9 @@ async function bootstrap() {
 
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.use(morgan('tiny', {
+    stream: logStream,
+  }));
   await app.listen(PORT);
   Logger.log(`Server start on host http://localhost:${PORT}/api`)
 }
