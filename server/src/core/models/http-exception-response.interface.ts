e97c70b1a3ae563bb250ef8c799223f792baf6c5
Taras Makarchuk <2278452@gmail.com>
2023-01-08 20:07:12 +0200

    :white_check_mark: add error details field in all-exceptions.filter.ts file, validate password length

--- server/src/core/models/http-exception-response.interface.ts
+++ server/src/core/models/http-exception-response.interface.ts
@@ -1,6 +1,7 @@
 export interface HttpExceptionResponse {
     statusCode: number;
     error: string;
+    errorDetails: string;
 }
 
 export interface CustomHttpExceptionResponse extends HttpExceptionResponse {
