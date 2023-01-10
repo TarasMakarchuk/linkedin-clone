65ac63ade326d464a43be4627a44b11842662f2d
Taras Makarchuk <2278452@gmail.com>
2023-01-10 20:54:25 +0200

    :construction: initialize tests in the post.controller.spec.ts file

--- server/src/post/post.controller.spec.ts
+++ server/src/post/post.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { UserService } from '../auth/services/user.service';
+import { JwtGuard } from '../auth/guards/jwt.guard';
+import { IsCreatorGuard } from './guards/is-creator.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+const httpMocks = require('node-mocks-http');
 
 describe('PostController', () => {
-  let controller: PostController;
+  let postController: PostController;
+  let postService: PostService;
+  let userService: UserService;
+
+  const mockPostService = {};
+  const mockUserService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
-    }).compile();
+      providers: [
+        PostService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+        {
+          provide: JwtGuard,
+          useValue: jest.fn().mockImplementation(() => true),
+        },
+        {
+          provide: IsCreatorGuard,
+          useValue: jest.fn().mockImplementation(() => true),
+        },
+        {
+          provide: RolesGuard,
+          useValue: jest.fn().mockImplementation(() => true),
+        },
+      ],
+    })
+        .overrideProvider(PostService)
+        .useValue(mockPostService)
+        .compile();
 
-    controller = module.get<PostController>(PostController);
+    postService = module.get<PostService>(PostService);
+    userService = module.get<UserService>(UserService);
+    postController = module.get<PostController>(PostController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('PostController should be defined', () => {
+    expect(postController).toBeDefined();
   });
 });
