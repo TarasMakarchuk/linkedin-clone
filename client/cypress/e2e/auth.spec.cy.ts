49114d6d29e8090b3d0aa373b17a23b5f968aaf0
Taras Makarchuk <2278452@gmail.com>
2023-01-21 17:16:50 +0200

    :ambulance:  fix auth tests

--- client/cypress/e2e/auth.spec.cy.ts
+++ client/cypress/e2e/auth.spec.cy.ts
@@ -1,7 +1,10 @@
+beforeEach(() => {
+  cy.visit('/');
+});
+
 describe('AuthModule', () => {
 
   it('should redirect to auth page if not signed in', () => {
-    cy.visit('/');
     cy.url().should('includes', 'auth');
   });
 
@@ -14,33 +17,44 @@ describe('AuthModule', () => {
   it('should have a disabled register button', () => {
     cy.get('ion-text.toggle-auth-mode').click();
     cy.get('ion-button')
-      .should('contain', 'Join')
+      .should('contain', 'Accept &')
       .should('have.attr', 'disabled');
   });
 
   it('should register and toggle to login form', () => {
+    cy.get('ion-text.toggle-auth-mode').click();
     cy.fixture('user').then((newUser) => {
       const { firstName, lastName, email, password } = newUser;
+      console.log(firstName, lastName, email, password);
       cy.register(firstName, lastName, email, password);
       cy.get('ion-button')
-        .should('contain', 'Sign in');
+        .should('contain', 'Accept &');
     });
   });
 
   it('should login and go to /home', () => {
-    cy.fixture('user').then(() => {
+    cy.fixture('user').then((user) => {
+      const { email, password } = user;
+      cy.login(email, password);
       cy.get('ion-button').should('not.have.attr', 'disabled');
       cy.get('ion-button').click();
+
       cy.url().should('not.include', 'auth');
       cy.url().should('includes', 'home');
+      cy.wait(2000);
     });
   });
 
   it('should logout and go to /auth login page', () => {
-    cy.get('ion-col.popover-menu').click();
-    cy.get('p.sign-out').click();
-    cy.url().should('not.include', 'home');
-    cy.url().should('includes', 'auth');
-  });
+    cy.fixture('user').then((user) => {
+      const {email, password} = user;
+      cy.login(email, password);
+      cy.get('ion-button').click();
 
+      cy.get('ion-col.popover-menu').click();
+        cy.get('p.sign-out').click();
+        cy.url().should('not.include', 'home');
+        cy.url().should('includes', 'auth');
+    });
+  });
 });
