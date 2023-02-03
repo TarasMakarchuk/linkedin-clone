311529584cd496c4a6e72bae7dc8395287c8c16f
Taras Makarchuk <2278452@gmail.com>
2023-02-03 18:07:32 +0200

    :bookmark: client - update   ionViewDidEnter, onSubmit, openConversation and ionViewDidLeave methods in chat.component.ts file

--- client/src/app/home/components/chat/chat.component.ts
+++ client/src/app/home/components/chat/chat.component.ts
@@ -68,25 +68,69 @@ export class ChatComponent {
       });
     });
 
+    this.newMessagesSubscription = this.chatService.getNewMessage().subscribe((message: Message) => {
+      message.created_at = new Date();
+
+      const allMessageIds = this.messages.map(
+        (message: Message) => message.id
+      );
+      if (!allMessageIds.includes(message.id)) {
+        this.messages.push(message)
+      }
+    });
+
+    this.friendsSubscription = this.friend$.subscribe((friend: any) => {
+      if (JSON.stringify(friend) !== '{}') {
+        this.chatService.joinConversation(this.friend.id);
+      }
+    });
+
     this.friendsSubscription = this.chatService.getFriends().subscribe((friends: User[]) => {
       this.friends = friends;
+      if (friends.length > 0) {
+        this.friend = this.friends[0];
+        this.friend$.next(this.friend);
+
+        friends.forEach((friend: User) => {
+          this.chatService.createConversation(friend);
+        });
+        this.chatService.joinConversation(this.friend.id);
+      }
     });
   };
 
   onSubmit() {
     const { message } = this.form.value;
     if (!message) return;
+
+    let conversationUserIds = [this.userId, this.friend.id].sort();
+
+    this.conversations.forEach((conversation: Conversation) => {
+      let userIds = conversation.users.map((user: User) => user.id).sort();
+
+      if (JSON.stringify(conversationUserIds) === JSON.stringify(userIds)) {
+        this.conversation = conversation;
+      }
+    });
+
     this.chatService.sendMessage(message, this.conversation);
     this.form.reset();
   };
 
   openConversation(friend: User, index: number): void {
     this.selectedConversationIndex = index;
+
+    this.chatService.leaveConversation();
+
     this.friend = friend;
     this.friend$.next(this.friend);
+
+    this.messages = [];
   };
 
   ionViewDidLeave() {
+    this.chatService.leaveConversation();
+
     this.userImagePathSubscription.unsubscribe();
     this.userIdSubscription.unsubscribe();
     this.newMessagesSubscription.unsubscribe();
